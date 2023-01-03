0405f271d4a0c60713cb67eb10745d2205220d01
AndreVeiga <soutomachado1@gmail.com>
2023-01-03 22:34:59 +0044

    Finalizando projeto

--- controllers/controller.go
+++ controllers/controller.go
@@ -10,9 +10,16 @@ import (
 )
 
 func ExibeTodosAlunos(c *gin.Context) {
+	cpf := c.Query("cpf")
+	
+	if len(cpf) > 0 {
+		BuscaAlunoPeloCPF(c, cpf)
+		return
+	}
+
 	var alunos []models.Aluno
 	bancoDados.DB.Find(&alunos)
-	c.JSON(200, alunos)
+	c.JSON(http.StatusOK, alunos)
 }
 
 func BuscaPeloId(c *gin.Context) {
@@ -43,4 +50,51 @@ func CriaNovoAluno(c *gin.Context) {
 	bancoDados.DB.Create(&aluno)
 
 	c.JSON(http.StatusCreated, aluno)
+}
+
+func DeletaAluno(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	bancoDados.DB.Delete(&aluno, id)
+
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func EditarAluno(c *gin.Context) {
+	var aluno models.Aluno
+
+	id := c.Params.ByName("id")
+
+	bancoDados.DB.First(&aluno, id)
+
+	if aluno.ID != 0 {
+		if err := c.ShouldBindJSON(&aluno); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		bancoDados.DB.Model(&aluno).UpdateColumns(aluno)
+
+		c.JSON(http.StatusOK, aluno)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Aluno não encontrado",
+		})
+	}
+}
+
+func BuscaAlunoPeloCPF(c *gin.Context, cpf string) {
+	var aluno models.Aluno
+
+	bancoDados.DB.Find(&aluno, "cpf = ?", cpf)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Aluno não encontrado",
+		})
+	} else {
+		c.JSON(http.StatusOK, aluno)
+	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,8 @@ import (
 
 func HandleRequest(e *gin.Engine) {
 	e.GET("/alunos", controllers.ExibeTodosAlunos)
-	e.GET("/alunos/:id", controllers.BuscaPeloId)
 	e.POST("/alunos", controllers.CriaNovoAluno)
+	e.GET("/alunos/:id", controllers.BuscaPeloId)
+	e.DELETE("/alunos/:id", controllers.DeletaAluno)
+	e.PATCH("/alunos/:id", controllers.EditarAluno)
 }
